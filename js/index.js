920217e6a6a57e452ad37bcd54ab239edf67e017
sushilcs111 <sushil.sud@gmail.com>
2017-06-05 18:57:47 +0530

    hgjhg

--- js/index.js
+++ js/index.js
@@ -5,6 +5,13 @@ var app = {
     onDeviceReady: function () {
        cordova.plugins.diagnostic.isExternalStorageAuthorized(function(authorized){
     alert("App is " + (authorized ? "authorized" : "denied") + " access to the external storage");
+    cordova.plugins.diagnostic.getExternalSdCardDetails(function(data){
+        alert(JSON.stringify(data))
+    }, function(err){
+        alert(err)
+    });
+
+
 }, function(error){
     alert("The following error occurred: "+error);
 });
