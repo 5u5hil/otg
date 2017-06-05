801384309fac5d4abd132bcfc29a0264b34bacab
sushilcs111 <sushil.sud@gmail.com>
2017-06-05 18:15:34 +0530

    asdasd

--- js/index.js
+++ js/index.js
@@ -3,9 +3,25 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function () {
-        chrome.usb.getDevices(function (devices) {
-            alert(JSON.stringify(devices))
-        });
+      serial.requestPermission(function success(){
+          alert("Success");
+      }, function error(){
+          alert("Error");
+      });
+      
+      serial.open({}, function success(){
+           alert("Success 2");
+      }, function error(){
+           alert("Error 2");
+      });
+      
+      serial.read(function success(buffer){
+           alert(JSON.stringify(buffer));
+      }, function error(){
+           alert("Error 3");
+      });
+
+
     }
 };
 
